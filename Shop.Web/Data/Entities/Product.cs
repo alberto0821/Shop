8cdaa61b2a3bf8fe19f138845154b44122054655
alberto0821 <52586481+alberto0821@users.noreply.github.com>
2019-07-19 12:38:22 -0500

    Adicionamos Products Controller

---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = false)]
+
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+
+        public DateTime LastSlae { get; set; }
+
+        [Display(Name = "Is Availabe")]
+
+        public DateTime IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+
+        public decimal Stock { get; set; }
+    }
+}
---
+++ Shop.Web/Migrations/20190718231523_InitialDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Web.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    LastPurchase = table.Column<DateTime>(nullable: false),
+                    LastSlae = table.Column<DateTime>(nullable: false),
+                    IsAvailabe = table.Column<DateTime>(nullable: false),
+                    Stock = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
